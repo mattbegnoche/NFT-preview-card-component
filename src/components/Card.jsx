0d74ec0315370a2e7b2afcf661bf781cdfe68efd
Matthew Begnoche <b3gnoche@gmail.com>
2026-01-19 01:46:07 -0600

    Add Outfit font and improve card component styling
    Added the Outfit variable font and integrated it via @font-face in index.css. Updated Card, PriceInfo, and TimeInfo components for improved layout, spacing, and color styling, including enhanced author section and text color adjustments.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,18 +17,25 @@ function Card({
         src={imageUrl}
         alt="Image of equilibrium cube"
       />
-      <div>
+      <div className="flex flex-col gap-4">
         <h1 className="text-xl font-semibold leading-7">{titleText}</h1>
         <p>{textContent}</p>
       </div>
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row   justify-between">
         <PriceInfo ETH={ETH} />
         <TimeInfo daysLeft={daysLeft} />
       </div>
       <div className="w-full h-px bg-blue-800"></div>
-      <div className="flex flex-row items-center gap-5">
-        <img className="w-10" src={authorImage} alt="author image" />
-        <span>Creation of {authorName}</span>
+      <div className="flex flex-row items-center gap-4">
+        <img
+          className="w-10 rounded-full border-[1.5px] border-color-white"
+          src={authorImage}
+          alt="author image"
+        />
+        <span>
+          <span className="text-blue-500">Creation of </span>
+          {authorName}
+        </span>
       </div>
     </figure>
   );
--- src/components/PriceInfo.jsx
+++ src/components/PriceInfo.jsx
@@ -1,6 +1,6 @@
 function PriceInfo({ ETH }) {
   return (
-    <div className="flex flex-row gap-1 items-center text-cyan-400 font-semibold">
+    <div className="flex flex-row gap-2 items-center text-cyan-400 font-semibold">
       <svg width="11" height="18" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M11 10.216 5.5 18 0 10.216l5.5 3.263 5.5-3.262ZM5.5 0l5.496 9.169L5.5 12.43 0 9.17 5.5 0Z"
